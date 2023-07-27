2860c192f3dda5a6798f22ecb781efac0c183b28
ManuelSeisdedos <seisdedosmanuel2@gmail.com>
2023-07-27 20:27:30 -0300

    prueba

--- app.js
+++ app.js
@@ -10,7 +10,15 @@ const app = express()
 app.use(express.json())
 app.use(morgan('dev'))
 
-app.use((0,cors.default)());
+app.use((req,res,next) => {
+    req.header("Acces-Control-Allow-Origin","*");
+    req.header("Access-Control-Allow-Credentials", "true");
+    req.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
 
 
 app.use('/', routes)
