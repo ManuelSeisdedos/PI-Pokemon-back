0d856e72991b232222aefe88d4f11ab38cd1f36c
ManuelSeisdedos <seisdedosmanuel2@gmail.com>
2023-07-28 17:13:04 -0300

    prueba

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 'use strict'
-import * as mongo from "./configs/mongoConfig.js"
 import mongoose from "mongoose"
 import app from "./app.js"
-const PORT = process.env.PORT || 3000
+const mongoUser = process.env.MONGO_USERNAME
+const mongoPass = process.env.MONGO_PASSWORD
+const PORT = process.env.PORT
 
 try { 
-    mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@pokemondata.dcme52o.mongodb.net/test`   
+    mongoose.connect(`mongodb+srv://${mongoUser}:${mongoPass}@pokemondata.dcme52o.mongodb.net/test`   
 )
     console.log("La conexión a la base de datos se realizó correctamente.")
 } catch (error) {
