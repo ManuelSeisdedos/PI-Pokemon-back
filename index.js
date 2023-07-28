543fa12006d853de8c6bac007c01b55799c9a72c
ManuelSeisdedos <seisdedosmanuel2@gmail.com>
2023-07-27 21:04:56 -0300

    prueba

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import app from "./app.js"
 const PORT = process.env.PORT || 3000
 
 try { 
-    mongoose.connect(`mongodb+srv://admin:pokemonback123@pokemondata.dcme52o.mongodb.net/`   
+    mongoose.connect(`mongodb+srv://adminotto:pokemonback123@pokemondata.dcme52o.mongodb.net/`   
 )
     console.log("La conexión a la base de datos se realizó correctamente.")
 } catch (error) {
